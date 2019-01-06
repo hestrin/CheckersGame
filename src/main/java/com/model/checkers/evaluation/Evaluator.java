e53da3342a1dc439b85b375d2808c3e8f83b21f6
mateusz.malinowski <matmmal@gmail.com>
2019-01-06 22:40:38 +0100

    Three - double step prediction for black and correction in assesment function. TODO - add cutting of decision tree ...

--- src/main/java/com/model/checkers/evaluation/Evaluator.java
+++ src/main/java/com/model/checkers/evaluation/Evaluator.java
@@ -13,12 +13,49 @@ public class Evaluator {
     private static final float MAX_EVAL = 25f;
 
     public static Move getBestMove(Game game) {
-        List<MoveEvaluation> evaluations = fourStepEvaluation(game, MoveAnalyzer.getAllowedMoves(game));
+        List<MoveEvaluation> evaluations = game.getPlayerOnTheMove().equals(Player.BLACK) ?
+                sixStepEvaluation(game, MoveAnalyzer.getAllowedMoves(game)) :
+                fourStepEvaluation(game, MoveAnalyzer.getAllowedMoves(game));
         sortEvaluationsForPlayer(evaluations, game.getPlayerOnTheMove());
-//        System.out.println("Evaluations for player " + game.getPlayerOnTheMove());
-//        evaluations.forEach(e -> System.out.println(e.toString()));
         return evaluations.get(0).getMove();
-//        getBestMoveEvaluationForPlayer(evaluations, game.getPlayerOnTheMove()).getMove();
+    }
+
+    public static List<MoveEvaluation> sixStepEvaluation(Game position, List<Move> moves) {
+        List<MoveEvaluation> evaluations = new ArrayList<>();
+        for(Move move : moves) {
+//            System.out.println( " STEP 6 :" + move.toString());
+            final Game copy = MoveAnalyzer.makeMove(new Game(position), move);
+            List<Move> responses = MoveAnalyzer.getAllowedMoves(copy);
+            if(responses != null && !responses.isEmpty()) {
+                MoveEvaluation bestResponse = getBestMoveEvaluationForPlayer(
+                        fiveStepEvaluation(copy, responses), copy.getPlayerOnTheMove());
+                evaluations.add(new MoveEvaluation(move, bestResponse.getEvaluation()));
+            }
+            else
+            {
+                evaluations.add(new MoveEvaluation(move, position.getPlayerOnTheMove().getDirection()*MAX_EVAL));
+            }
+        }
+        return evaluations;
+    }
+
+    public static List<MoveEvaluation> fiveStepEvaluation(Game position, List<Move> moves) {
+        List<MoveEvaluation> evaluations = new ArrayList<>();
+        for(Move move : moves) {
+//            System.out.println( " STEP 5 :" + move.toString());
+            final Game copy = MoveAnalyzer.makeMove(new Game(position), move);
+            List<Move> responses = MoveAnalyzer.getAllowedMoves(copy);
+            if(responses != null && !responses.isEmpty()) {
+                MoveEvaluation bestResponse = getBestMoveEvaluationForPlayer(
+                        fourStepEvaluation(copy, responses), copy.getPlayerOnTheMove());
+                evaluations.add(new MoveEvaluation(move, bestResponse.getEvaluation()));
+            }
+            else
+            {
+                evaluations.add(new MoveEvaluation(move, position.getPlayerOnTheMove().getDirection()*MAX_EVAL));
+            }
+        }
+        return evaluations;
     }
 
     public static List<MoveEvaluation> fourStepEvaluation(Game position, List<Move> moves) {
@@ -97,24 +134,31 @@ public class Evaluator {
                 player.getDirection());
     }
 
-    public static float asses(Game postion) {
+    public static float asses(Game position) {
         int whitePieces = 0;
         int blackPieces = 0;
+        int whiteAdancements = 0;
+        int blackAdancements = 0;
+
         for(int i=0; i<8; i++) {
             for(int j=0; j<8; j++) {
-                if(postion.getBoard()[i][j].isOccupiedBy(Player.WHITE)) {
+                if(position.getBoard()[i][j].isOccupiedBy(Player.WHITE)) {
                     whitePieces++;
-                    if(postion.getBoard()[i][j].getPiece().isPromoted())
+                    if(position.getBoard()[i][j].getPiece().isPromoted())
                         whitePieces++;
+                    else
+                        whiteAdancements += i;
                 }
-                else if(postion.getBoard()[i][j].isOccupiedBy(Player.BLACK))
+                else if(position.getBoard()[i][j].isOccupiedBy(Player.BLACK))
                 {
                     blackPieces++;
-                    if(postion.getBoard()[i][j].getPiece().isPromoted())
+                    if(position.getBoard()[i][j].getPiece().isPromoted())
                         blackPieces++;
+                    else
+                        blackAdancements += 7-i;
                 }
             }
         }
-        return whitePieces-blackPieces;
+        return (whitePieces-blackPieces) + 0.01f *(whiteAdancements-blackAdancements);
     }
 }
